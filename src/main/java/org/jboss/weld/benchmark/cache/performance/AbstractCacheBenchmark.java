4c4b6dd627500868a83b87c7e961da77d93450af
Martin Kouba <mkouba@redhat.com>
2014-11-03 14:32:30 +0100

    Configurable number of threads and key computation complexity

--- src/main/java/org/jboss/weld/benchmark/cache/performance/AbstractCacheBenchmark.java
+++ src/main/java/org/jboss/weld/benchmark/cache/performance/AbstractCacheBenchmark.java
@@ -16,6 +16,8 @@
  */
 package org.jboss.weld.benchmark.cache.performance;
 
+import java.math.BigInteger;
+
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
@@ -25,15 +27,26 @@ import org.openjdk.jmh.annotations.State;
 @State(Scope.Benchmark)
 public abstract class AbstractCacheBenchmark<C> {
 
+    protected final Long complexity;
+
     private final C cache;
 
     protected AbstractCacheBenchmark() {
+        this.complexity = Long
+                .getLong("org.jboss.weld.benchmark.cache.complexity");
         this.cache = getCache();
     }
 
     protected abstract C getCache();
 
     protected abstract Foo getValue(C cache, Foo key);
+
+    protected Foo compute(Foo key) {
+        if (complexity != null) {
+            factorial((complexity + (key.getValue() % 10)));
+        }
+        return key;
+    }
 
     protected int getMaxSize() {
         return 1 << 16;
@@ -44,12 +57,19 @@ public abstract class AbstractCacheBenchmark<C> {
         long counter = 0;
     }
 
-
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     public boolean testGetValue(Counter counter) {
         Foo key = new Foo(counter.counter++);
         Foo value = getValue(cache, key);
         return key == value;
+    }
+
+    private BigInteger factorial(long input) {
+        BigInteger result = BigInteger.ONE;
+        for (long i = input; i > 1; i--) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
     }
 }
